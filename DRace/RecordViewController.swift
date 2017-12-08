bac1095af564cb7e052c33711f1c094f8ea0e81d
wongunyoung <vlzkcb1633@naver.com>
2017-12-08 12:59:30 +0900

    로컬 푸시기능 추가. 매일 반복알림 시작됨

--- DRace/RecordViewController.swift
+++ DRace/RecordViewController.swift
@@ -9,14 +9,41 @@
 import Foundation
 import UIKit
 import Firebase
+import UserNotifications
+import FirebaseDatabase
+import FirebaseAuth
 
-class RecordViewController: UIViewController {
+class RecordViewController: UIViewController, UNUserNotificationCenterDelegate {
     
     @IBOutlet weak var exerdata: UITextView!
     @IBOutlet weak var weightdata: UITextView!
     
+    // lastex에 마지막 운동량 집어넣음
+    let lastex = 200
+    let user = Auth.auth().currentUser
+    
     func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
         print("Firebase registration token: \(fcmToken)")
+    }
+    
+    
+    // 푸시알림. 로컬로 담당.
+    func setnoti(){
+        let content = UNMutableNotificationContent()
+        content.title = "Noti Title"
+        content.body = "어제의 운동량은 총 \(lastex/60)시간 \(lastex%60)분입니다!"
+        
+        var dateComponents = DateComponents()
+        // 시간 분 입력. 매일. 반복됨
+        dateComponents.hour = 1
+        dateComponents.minute = 24
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let request = UNNotificationRequest(identifier: "time", content: content, trigger: trigger)
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error) in
+            print(error?.localizedDescription ?? "")
+        }
     }
     
     override func viewDidLoad() {
@@ -26,6 +53,7 @@ class RecordViewController: UIViewController {
         exerdata.backgroundColor = UIColor(red: 255, green: 195, blue: 190, alpha: 1)
         weightdata.text = "현재 몸무게 10kg입니다."
         weightdata.backgroundColor = UIColor(red: 255, green: 195, blue: 190, alpha: 1)
+        setnoti()
     }
     
     override func didReceiveMemoryWarning() {
